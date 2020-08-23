44bf2f05d087a02e72cac7b64d5c8329142da84f
Turzoxpress <turzoxpress@gmail.com>
2020-08-23 17:27:48 +0600

    user profile & address api

--- web/app.py
+++ web/app.py
@@ -59,7 +59,9 @@ class RegisterSuperAdmin(Resource):
         # Store username and pw into the database
         superad.insert_one({
             "email": email,
-            "password": hashed_pw
+            "password": hashed_pw,
+            "role": "Super Admin",
+            "created_at": datetime.today().strftime('%d-%m-%Y')
 
         })
 
@@ -164,11 +166,15 @@ class SuperAdminLogin(Resource):
             }
             return jsonify(retJson)
 
+        userid = superad.find({
+            "email": email
+        })[0]["_id"]
+
         # -- Generate an access token
         retJson = {
             'status': "ok",
             'msg': {
-                "id": 2,
+                "id": str(userid),
                 "token": generateAuthToken(email)
             }
         }
@@ -318,7 +324,10 @@ class UpdateSuperAdminPassword(Resource):
                     hashed_pw = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
 
                     myquery = {"email": which_user}
-                    newvalues = {"$set": {"password": hashed_pw}}
+                    newvalues = {"$set": {
+                        "password": hashed_pw,
+                        "updated_at": datetime.today().strftime('%d-%m-%Y')
+                    }}
 
                     superad.update_one(myquery, newvalues)
 
@@ -358,6 +367,464 @@ class UpdateSuperAdminPassword(Resource):
             return jsonify(retJson)
 
 
+# -- Super Admin Profile Info Update
+class SuperAdminProfileInfoUpdate(Resource):
+    def post(self):
+        auth_header_value = request.headers.get('Authorization', None)
+
+        if not auth_header_value:
+            # return False
+            retJson = {
+                "status": "failed",
+                "msg": "Invalid access token"
+            }
+
+            return jsonify(retJson)
+
+        parts = auth_header_value.split()
+
+        if parts[0].lower() != 'bearer':
+            # return False
+            retJson = {
+                "status": "failed",
+                "msg": "Invalid access token"
+            }
+
+            return jsonify(retJson)
+        elif len(parts) == 1:
+            # return False
+            retJson = {
+                "status": "failed",
+                "msg": "Invalid access token"
+            }
+
+            return jsonify(retJson)
+        elif len(parts) > 2:
+            # return False
+            retJson = {
+                "status": "failed",
+                "msg": "Invalid access token"
+            }
+
+            return jsonify(retJson)
+
+        try:
+            # *******************************************
+            # *******************************************
+            payload = jwt.decode(parts[1], str(secret_key), algorithms='HS256')
+            # return payload['sub']
+            which_user = payload['sub']
+
+            # Check user with email
+            if not UserExist(which_user):
+                retJson = {
+                    "status": "failed",
+                    "msg": "Invalid access token"
+                }
+
+                return jsonify(retJson)
+
+            # get the data
+            postedData = request.get_json()
+
+            # Get the data
+            fname = postedData["fname"]
+            lname = postedData["lname"]
+            mobile = postedData["mobile"]
+            date_of_birth = postedData["date_of_birth"]
+            place_of_birth = postedData["place_of_birth"]
+            gender = postedData["gender"]
+            marital_status = postedData["marital_status"]
+            nationality = postedData["nationality"]
+            nid = postedData["nid"]
+            religion = postedData["religion"]
+            designation = postedData["designation"]
+
+            myquery = {"email": which_user}
+            newvalues = {"$set": {
+                "username": fname,
+                "fname": fname,
+                "lname": lname,
+                "mobile": mobile,
+                "date_of_birth": date_of_birth,
+                "place_of_birth": place_of_birth,
+                "marital_status": marital_status,
+                "nationality": nationality,
+                "nid": nid,
+                "gender": gender,
+                "religion": religion,
+                "designation": designation,
+                "updated_at": datetime.today().strftime('%d-%m-%Y')
+            }}
+
+            superad.update_one(myquery, newvalues)
+
+            retJson = {
+                "status": "ok",
+                "msg": "Profile info updated"
+            }
+            return jsonify(retJson)
+
+
+        # ********************************************************************************************************
+        # ********************************************************************************************************
+
+        except jwt.ExpiredSignatureError:
+            # return 'Signature expired. Please log in again.'
+            retJson = {
+                "status": "failed",
+                "msg": "Invalid access token"
+            }
+
+            return jsonify(retJson)
+
+        except jwt.InvalidTokenError:
+            # return 'Invalid token. Please log in again.'
+            retJson = {
+                "status": "failed",
+                "msg": "Invalid access token"
+            }
+
+            return jsonify(retJson)
+
+
+# -- Get Super Admin profile info
+class GetSuperAdminProfileInfo(Resource):
+    def get(self):
+        auth_header_value = request.headers.get('Authorization', None)
+
+        if not auth_header_value:
+            # return False
+            retJson = {
+                "status": "failed",
+                "msg": "Invalid access token"
+            }
+
+            return jsonify(retJson)
+
+        parts = auth_header_value.split()
+
+        if parts[0].lower() != 'bearer':
+            # return False
+            retJson = {
+                "status": "failed",
+                "msg": "Invalid access token"
+            }
+
+            return jsonify(retJson)
+        elif len(parts) == 1:
+            # return False
+            retJson = {
+                "status": "failed",
+                "msg": "Invalid access token"
+            }
+
+            return jsonify(retJson)
+        elif len(parts) > 2:
+            # return False
+            retJson = {
+                "status": "failed",
+                "msg": "Invalid access token"
+            }
+
+            return jsonify(retJson)
+
+        try:
+            # *******************************************
+            # *******************************************
+            payload = jwt.decode(parts[1], str(secret_key), algorithms='HS256')
+            # return payload['sub']
+            which_user = payload['sub']
+
+            # Check user with email
+            if not UserExist(which_user):
+                retJson = {
+                    "status": "failed",
+                    "msg": "Invalid access token"
+                }
+
+                return jsonify(retJson)
+
+            result = superad.find({"email": which_user})
+            holder = []
+            user_data = {}
+            for i in result:
+                #user_data = {}
+                user_data["id"] = str(i["_id"])
+                user_data["username"] = str(i["username"])
+                user_data["email"] = str(i["email"])
+                user_data["avatar_img"] = ""
+                user_data["cover_img"] = ""
+                user_data["created_at"] = str(i["created_at"])
+                user_data["fname"] = str(i["fname"])
+                user_data["lname"] = str(i["lname"])
+                user_data["mobile"] = str(i["mobile"])
+                user_data["marital_status"] = str(i["marital_status"])
+                user_data["date_of_birth"] = str(i["date_of_birth"])
+                user_data["place_of_birth"] = str(i["place_of_birth"])
+                user_data["gender"] = str(i["gender"])
+                user_data["religion"] = str(i["religion"])
+                user_data["nationality"] = str(i["nationality"])
+                user_data["nid"] = str(i["nid"])
+                user_data["designation"] = str(i["designation"])
+                user_data["role"] = str(i["role"])
+                #holder.append(user_data)
+
+            retJson = {
+                "status": "ok",
+                "msg": user_data
+            }
+            return jsonify(retJson)
+
+
+        # ********************************************************************************************************
+        # ********************************************************************************************************
+
+        except jwt.ExpiredSignatureError:
+            # return 'Signature expired. Please log in again.'
+            retJson = {
+                "status": "failed",
+                "msg": "Invalid access token"
+            }
+
+            return jsonify(retJson)
+
+        except jwt.InvalidTokenError:
+            # return 'Invalid token. Please log in again.'
+            retJson = {
+                "status": "failed",
+                "msg": "Invalid access token"
+            }
+
+            return jsonify(retJson)
+
+
+# -- Super Admin Address Update
+class SuperAdminAddressUpdate(Resource):
+    def post(self):
+        auth_header_value = request.headers.get('Authorization', None)
+
+        if not auth_header_value:
+            # return False
+            retJson = {
+                "status": "failed",
+                "msg": "Invalid access token"
+            }
+
+            return jsonify(retJson)
+
+        parts = auth_header_value.split()
+
+        if parts[0].lower() != 'bearer':
+            # return False
+            retJson = {
+                "status": "failed",
+                "msg": "Invalid access token"
+            }
+
+            return jsonify(retJson)
+        elif len(parts) == 1:
+            # return False
+            retJson = {
+                "status": "failed",
+                "msg": "Invalid access token"
+            }
+
+            return jsonify(retJson)
+        elif len(parts) > 2:
+            # return False
+            retJson = {
+                "status": "failed",
+                "msg": "Invalid access token"
+            }
+
+            return jsonify(retJson)
+
+        try:
+            # *******************************************
+            # *******************************************
+            payload = jwt.decode(parts[1], str(secret_key), algorithms='HS256')
+            # return payload['sub']
+            which_user = payload['sub']
+
+            # Check user with email
+            if not UserExist(which_user):
+                retJson = {
+                    "status": "failed",
+                    "msg": "Invalid access token"
+                }
+
+                return jsonify(retJson)
+
+            # get the data
+            postedData = request.get_json()
+
+            # Get the data
+            address = postedData["address"]
+            post_office = postedData["post_office"]
+            post_code = postedData["post_code"]
+            thana = postedData["thana"]
+            district = postedData["district"]
+            division = postedData["division"]
+            per_address = postedData["per_address"]
+            per_post_office = postedData["per_post_office"]
+            per_post_code = postedData["per_post_code"]
+            per_thana = postedData["per_thana"]
+            per_district = postedData["per_district"]
+            per_division = postedData["per_division"]
+
+            myquery = {"email": which_user}
+            newvalues = {"$set": {
+                "address": address,
+                "post_office": post_office,
+                "post_code": post_code,
+                "thana": thana,
+                "district": district,
+                "division": division,
+                "per_address": per_address,
+                "per_post_office": per_post_office,
+                "per_post_code": per_post_code,
+                "per_thana": per_thana,
+                "per_district": per_district,
+                "per_division": per_division,
+                "updated_at": datetime.today().strftime('%d-%m-%Y')
+            }}
+
+            superad.update_one(myquery, newvalues)
+
+            retJson = {
+                "status": "ok",
+                "msg": "Address updated"
+            }
+            return jsonify(retJson)
+
+
+        # ********************************************************************************************************
+        # ********************************************************************************************************
+
+        except jwt.ExpiredSignatureError:
+            # return 'Signature expired. Please log in again.'
+            retJson = {
+                "status": "failed",
+                "msg": "Invalid access token"
+            }
+
+            return jsonify(retJson)
+
+        except jwt.InvalidTokenError:
+            # return 'Invalid token. Please log in again.'
+            retJson = {
+                "status": "failed",
+                "msg": "Invalid access token"
+            }
+
+            return jsonify(retJson)
+
+# -- Get Super Admin address
+class GetSuperAdminAddress(Resource):
+    def get(self):
+        auth_header_value = request.headers.get('Authorization', None)
+
+        if not auth_header_value:
+            # return False
+            retJson = {
+                "status": "failed",
+                "msg": "Invalid access token"
+            }
+
+            return jsonify(retJson)
+
+        parts = auth_header_value.split()
+
+        if parts[0].lower() != 'bearer':
+            # return False
+            retJson = {
+                "status": "failed",
+                "msg": "Invalid access token"
+            }
+
+            return jsonify(retJson)
+        elif len(parts) == 1:
+            # return False
+            retJson = {
+                "status": "failed",
+                "msg": "Invalid access token"
+            }
+
+            return jsonify(retJson)
+        elif len(parts) > 2:
+            # return False
+            retJson = {
+                "status": "failed",
+                "msg": "Invalid access token"
+            }
+
+            return jsonify(retJson)
+
+        try:
+            # *******************************************
+            # *******************************************
+            payload = jwt.decode(parts[1], str(secret_key), algorithms='HS256')
+            # return payload['sub']
+            which_user = payload['sub']
+
+            # Check user with email
+            if not UserExist(which_user):
+                retJson = {
+                    "status": "failed",
+                    "msg": "Invalid access token"
+                }
+
+                return jsonify(retJson)
+
+            result = superad.find({"email": which_user})
+            holder = []
+            user_data = {}
+            for i in result:
+                #user_data = {}
+                user_data["id"] = str(i["_id"])
+                user_data["user_id"] = str(i["_id"])
+                user_data["address"] = str(i["address"])
+                user_data["post_office"] = str(i["post_office"])
+                user_data["post_code"] = str(i["post_code"])
+                user_data["thana"] = str(i["thana"])
+                user_data["district"] = str(i["district"])
+                user_data["division"] = str(i["division"])
+                user_data["per_address"] = str(i["per_address"])
+                user_data["per_post_office"] = str(i["per_post_office"])
+                user_data["per_post_code"] = str(i["per_post_code"])
+                user_data["per_thana"] = str(i["per_thana"])
+                user_data["per_district"] = str(i["per_district"])
+                user_data["per_division"] = str(i["per_division"])
+                #holder.append(user_data)
+
+            retJson = {
+                "status": "ok",
+                "msg": user_data
+            }
+            return jsonify(retJson)
+
+
+        # ********************************************************************************************************
+        # ********************************************************************************************************
+
+        except jwt.ExpiredSignatureError:
+            # return 'Signature expired. Please log in again.'
+            retJson = {
+                "status": "failed",
+                "msg": "Invalid access token"
+            }
+
+            return jsonify(retJson)
+
+        except jwt.InvalidTokenError:
+            # return 'Invalid token. Please log in again.'
+            retJson = {
+                "status": "failed",
+                "msg": "Invalid access token"
+            }
+
+            return jsonify(retJson)
 # -----------------------------------------------------------------------
 
 
@@ -369,6 +836,10 @@ api.add_resource(DeleteAllData, '/delete_all_data')
 api.add_resource(SuperAdminLogin, '/super_admin_login')
 api.add_resource(SuperAdminLogOut, '/super_admin_logout')
 api.add_resource(UpdateSuperAdminPassword, '/super_admin_password_update')
+api.add_resource(SuperAdminProfileInfoUpdate, '/super_admin_profile_info_update')
+api.add_resource(GetSuperAdminProfileInfo, '/get_super_admin_profile_info')
+api.add_resource(SuperAdminAddressUpdate, '/super_user_address_update')
+api.add_resource(GetSuperAdminAddress, '/get_super_admin_address')
 
 # -----------------------------------------------------------------------
 
